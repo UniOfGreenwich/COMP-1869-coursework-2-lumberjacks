c6f70885b7e6cb7e842e7f1cf720e15360c08659
jamielill <jl2592c@gre.ac.uk>
2025-11-11 21:31:42 +0000

    made the stock market chart work with the simulated dataset

--- UOG-FYGP-Lumberjack-Abi/Assets/Scripts/StockMarket/StockMarketChart.cs
+++ UOG-FYGP-Lumberjack-Abi/Assets/Scripts/StockMarket/StockMarketChart.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class StockMarketChart : MonoBehaviour
 {
+    /*
+    Get array size of realWorldData then - timePeriod and store as startTime
+    use this to generate the last 12 hours of candlesticks
+    Take the abs(open-close) to get the height of the candlestick
+    take open+(open-close)/2 to position to generate the candlestick
+    Repeat 11 times and add width each time
+    */
+
     [SerializeField] List<Image> candleSticks;
+    [SerializeField] private int timePeriod = 12;
+    private int startTime;
+    private float height;
+    private float position;
+    [SerializeField] private float positionOffset;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        startTime = SimulatedRealWorldDataSet.tradeData.GetLength(0) - timePeriod;
         GenerateCandleSticks();
     }
 
@@ -19,8 +34,8 @@ public class StockMarketChart : MonoBehaviour
     }
 
     void GenerateCandleSticks()
-    {
-        for(int i = 0; i < 12; i++)
+    {   
+        for(int i=0;i < timePeriod; i++)
         {
             GameObject candleStick = new GameObject("CandleStick");    
             candleStick.transform.SetParent(this.transform);
@@ -29,14 +44,14 @@ public class StockMarketChart : MonoBehaviour
             Image image = candleStick.AddComponent<Image>();
             RectTransform rectTransform = candleStick.GetComponent<RectTransform>();
             candleSticks.Add(image);
-            candleSticks[i].rectTransform.anchoredPosition = new Vector2(i*50-600, 0);
 
-            //random height between 50 and 500
-            float height = Random.Range(50, 500);
-            candleSticks[i].rectTransform.sizeDelta = new Vector2(25, height);
+            height = SimulatedRealWorldDataSet.tradeData[startTime+i, 1] - SimulatedRealWorldDataSet.tradeData[startTime+i, 0];
+            position = SimulatedRealWorldDataSet.tradeData[startTime+i, 0] + height/2;
+            candleSticks[i].rectTransform.sizeDelta = new Vector2(25, Mathf.Abs(height));
+            candleSticks[i].rectTransform.anchoredPosition = new Vector2(i * 25 - 600, position-positionOffset);
             
-            //random color red or green
-            if (Random.value > 0.5f)
+            //red or green candlestick based on increase or decrease in value
+            if (height>0)
             {
                 candleSticks[i].color = Color.green;
             }
