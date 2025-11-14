ae95165b1321ae40f13cd24657a1d685581e4121
jamielill <118823349+jamielill@users.noreply.github.com>
2025-11-14 10:35:07 +0000

    added settings code

---
+++ UOG-FYGP-Lumberjack-Abi/Assets/Scripts/Stats/Settings.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] private int volumeMaster;
+    [SerializeField] private int volumeSFX;
+    [SerializeField] private int volumeMusic;
+    [SerializeField] private bool notifications;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
