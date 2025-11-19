495e456ee897050e4d32ecca44c884d2a5c07f0d
Abishek Ramalingam <ar8665r@gre.ac.uk>
2025-11-19 02:47:15 +0000

    mm
    mm

--- UOG-FYGP-Lumberjack-Abi/Assets/Scripts/ProductionMachine.cs
+++ UOG-FYGP-Lumberjack-Abi/Assets/Scripts/ProductionMachine.cs
@@ -1,51 +1,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Placeble))]
 public class ProductionMachine : MonoBehaviour
 {
-    public GameObject uiPrefab;
+    public ProductionMachineUI ui;
     public List<ProductionRecipeSO> recipes = new List<ProductionRecipeSO>();
 
-    ProductionMachineUI uiInstance;
     StorageManager storage;
-    Placeble placeble;
 
     void Awake()
     {
         storage = Object.FindAnyObjectByType<StorageManager>();
-        placeble = GetComponent<Placeble>();
     }
 
     void OnMouseDown()
     {
-        if (placeble != null && !placeble.placed) return;
-        OpenUI();
-    }
-
-    void OpenUI()
-    {
-        if (!uiPrefab) return;
-
-        if (uiInstance == null)
-        {
-            Transform parent = GameManager.current ? GameManager.current.canvas.transform : null;
-            GameObject go = Instantiate(uiPrefab, parent);
-            uiInstance = go.GetComponent<ProductionMachineUI>();
-            uiInstance.Init(this, recipes);
-        }
-        else
-        {
-            uiInstance.gameObject.SetActive(true);
-        }
+        if (ui == null) return;
+        ui.gameObject.SetActive(true);
+        ui.Init(this, recipes);
     }
 
     public void OnAssemble(ProductionRecipeSO recipe, int errors)
     {
-        if (storage == null || recipe == null || recipe.finishedProduct == null) return;
+        if (storage == null) return;
+        if (recipe == null) return;
+        if (recipe.finishedProduct == null) return;
 
         storage.Put(recipe.finishedProduct, 1);
-
         string key = string.IsNullOrEmpty(recipe.id) ? recipe.name : recipe.id;
         Debug.Log("[ProductionMachine] Built " + key + " with errors " + errors);
     }
