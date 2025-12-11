b911908b2286511f322157917a36496dd3d630d8
jamielill <jl2592c@gre.ac.uk>
2025-12-11 18:31:53 +0000

    added tutorial code

---
+++ UOG-FYGP-Lumberjack-Abi/Assets/Prefabs/Menus/Tutorial/Tutorial.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //("Welcome to LumberJill's Carpenter Shop! Explore the workshop by clicking around, to go to another room walk over the yellow boxes on the floor.")
+}
