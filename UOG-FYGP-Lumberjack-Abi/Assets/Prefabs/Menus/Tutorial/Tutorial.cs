ac6b5b92034ba46ae8fa5745266670c6cda24502
jamielill <jl2592c@gre.ac.uk>
2025-12-12 13:43:45 +0000

    added some code to tutorial

--- UOG-FYGP-Lumberjack-Abi/Assets/Prefabs/Menus/Tutorial/Tutorial.cs
+++ UOG-FYGP-Lumberjack-Abi/Assets/Prefabs/Menus/Tutorial/Tutorial.cs
@@ -1,17 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    private TMP_Text tutorialText;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tutorialText = GetComponentInChildren<TMP_Text>();
+        Stage1();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Stage1()
     {
-        
+        tutorialText.text = "Welcome to LumberJill's Carpenter Shop! Explore the workshop by clicking around, to go to another room walk over the yellow boxes on the floor.";
+        // on click hide text, enable task panel
+        // once each room has been visited, show final message
+        // activate stage 2
+
     }
 
     //("Welcome to LumberJill's Carpenter Shop! Explore the workshop by clicking around, to go to another room walk over the yellow boxes on the floor.")
